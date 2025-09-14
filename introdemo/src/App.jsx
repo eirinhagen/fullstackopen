e5579c82ab7ce8e6caf4a2ccd37276393493b6f6
eirinhagen <142972367+sunwontrise@users.noreply.github.com>
2025-09-14 18:31:08 +0200

    Complex state

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,51 +1,40 @@
 import {useState} from "react";
 
-const Display = ({counter}) => {
-    return (
-        <div>{counter}</div>
-    )
+//Complex state
 
-}
-
-const Button = ({onClick,text}) => {
-    return (
-        <button onClick={onClick}>
-            {text}
-        </button>
-    )
-}
 const App = () => {
-    const [counter, setCounter] = useState(0);
-    console.log('rendering with counter value', counter)
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
 
-    //setTimeout(() => setCounter(counter + 1), 1000);
-    /* const handleClick = () => {
-         console.log("clicked");
-     }
-    console.log("rendering...", counter);*/
+    /*const handleLeftClick = () => {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right
+        }
+        setClicks(newClicks)
+    }
 
-    const increaseByOne = () => {
-        console.log('increasing, value before', counter)
-        setCounter(counter + 1);
-    }
-    const decreaseByOne = () => {
-        console.log('decreasing, value before', counter)
-        setCounter(counter - 1);
-    }
-    const setToZero = () => {
-        console.log('resetting to zero, value before', counter)
-        setCounter(0);
-    }
+    const handleRightClick = () => {
+        const newClicks = {
+            left: clicks.left,
+            right: clicks.right + 1
+        }
+        setClicks(newClicks)
+    }*/
+    const handleLeftClick = () =>
+        setClicks({ ...clicks, left: clicks.left + 1 })
+
+    const handleRightClick = () =>
+        setClicks({ ...clicks, right: clicks.right + 1 })
 
     return (
         <div>
-            <Display counter={counter}/>
-            <Button onClick={increaseByOne} text='plus'/>
-            <Button onClick={setToZero} text='zero'/>
-            <Button onClick={decreaseByOne} text='minus'/>
+            {clicks.left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {clicks.right}
         </div>
-
-
     )
 }
 
